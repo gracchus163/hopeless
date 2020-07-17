1c84760274dd5d8309521b7cf430865a5e2e2848
user <user@flatline.localdomain>
2020-07-17 21:50:14 +0100

    Putting basic pieces together before behaviour, based off nio_template

---
+++ bot_actions.py
@@ -0,0 +1,13 @@
+def valid_email(email):
+    if email == "true":
+        return True
+    else:
+        return False
+
+def valid_token(token):
+    if token == "123456":
+        return True
+    else:
+        False
+def invite(username):
+    print("inviting" + username)
--- bot_commands.py
+++ bot_commands.py
@@ -1,5 +1,5 @@
 from chat_functions import send_text_to_room
-
+from bot_actions import valid_email, valid_token
 
 class Command(object):
     def __init__(self, client, store, config, command, room, event):
@@ -32,9 +32,37 @@ class Command(object):
             await self._echo()
         elif self.command.startswith("help"):
             await self._show_help()
+        elif self.command.startswith("request"):
+            await self._process_request()
         else:
             await self._unknown_command()
 
+    async def _process_request(self):
+        """!h request $email $token"""
+        print("args are:" + " ".join([str(x) for x in self.args]))
+        email = str(self.args[0])
+        print("email: " + email)
+        if not valid_email(email):
+            response = "Not a valid email, try again. request arguments are: $email $token"
+            await send_text_to_room(self.client, self.room.room_id, response)
+            return
+        token = str(self.args[1])
+        if len(token) != 6:
+            response = "Token must be 36 characters, check your ticket again or contact valka"
+            await send_text_to_room(self.client, self.room.room_id, response)
+            return
+        if valid_token(token):
+            response = "Verified, congrats. You should now be invited to the official rooms"
+            await send_text_to_room(self.client, self.room.room_id, response)
+   #        invite(user)
+            return
+        else:
+            response = "This is not a valid token, check your ticket again or contact valka"
+            await send_text_to_room(self.client, self.room.room_id, response)
+            return
+
+
+    
     async def _echo(self):
         """Echo back the command's arguments"""
         response = " ".join(self.args)
