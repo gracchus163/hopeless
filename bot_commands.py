35f23509a40b77df88599bfc1b0623fdaacb6ace
Trevor Bergeron <mal@sec.gd>
2020-07-27 14:48:45 -0400

    fix invite command

--- bot_commands.py
+++ bot_commands.py
@@ -255,7 +255,7 @@ class Command(object):
             )
             await send_text_to_room(self.client, self.room.room_id, response)
             return
-        ret, room_id = get_roomid(self.client, self.args[1])
+        ret, room_id = await get_roomid(self.client, self.args[1])
         if not ret:
             response = "Could not find a roomid for that room name"
             await send_text_to_room(self.client, self.room.room_id, response)
