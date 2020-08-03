9cb4f5454e9920e218ce6912bd39dfa667f32076
Trevor Bergeron <mal@sec.gd>
2020-08-03 00:33:32 -0400

    fix config default=(falsy)

--- config.py
+++ config.py
@@ -183,7 +183,7 @@ class Config(object):
             # If at any point we don't get our expected option...
             if config is None:
                 # Raise an error if it was required
-                if required or not default:
+                if required or default is None:
                     raise ConfigError(f"Config option {'.'.join(path)} is required")
 
                 # or return the default value
